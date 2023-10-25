30d2ed2c80e9679f20b1e9e377ab1ec6c2c1ed02
Ezekiel Enns <Ezekiel.Enns@protonmail.com>
2023-10-24 22:33:06 -0300

    got a texture up just need to fix atlas

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,29 @@ const atlas_height = 64;
 const atlas_count = 94;
 //watch 
 //https://youtu.be/w3im_9qbM18?t=451
-
+//https://youtu.be/w3im_9qbM18?t=939
 //generating atlas
 const atlas = {};
 for (const x of Array(94).keys()){
-    let p = x/94.0
+    const col = x % atlas_columns;
+    const row = Math.floor(x/atlas_columns)
+
+    let u = (col * img_width)/atlas_width
+    let v = (row * img_height)/atlas_height
+    let w = img_width/atlas_width
+    let h = img_height/atlas_height
+
+    //@ts-ignore
+    atlas[String.fromCharCode(x+32)] = [
+        //6 verts
+        u, v+h,
+        u+w, v,
+        u,   v,
+
+        u,   v+h,
+        u+w, v+h,
+        u+w, v,
+    ]
 }
 //TODO rotate on loop
 //move tirangle around
@@ -22,11 +40,10 @@ const vertShaderSrc = `#version 300 es
 //precision mediump float;
 layout(location=0) in vec4 aPos;
 layout(location=1) in vec2 aTexCoord;
-uniform mat2 uRot;
 
 out vec2 vTexCoord;
 void main() {
-    gl_Position = vec4((aPos.xy * uRot),aPos.zw);
+    gl_Position = aPos;
     vTexCoord = aTexCoord;
 }
 `;
@@ -46,17 +63,12 @@ void main() {
 `;
 
 const data = new Float32Array([
-  -0.5, -0.5,   0, 0, 
-     0,  0.5,   0.5, 1, 
-   0.5, -0.5,    1, 0,
-]);
-
-
-//temparay texture
-const pixels = new Uint8Array([
-  255, 255, 255, 230, 25, 75, 60, 180, 75, 255, 225, 25, 67, 99, 216, 245, 130,
-  49, 145, 30, 180, 70, 240, 240, 240, 50, 230, 188, 246, 12, 250, 190, 190, 0,
-  128, 128, 230, 190, 255, 154, 99, 36, 255, 250, 200, 0, 0, 0,
+    -1,0,  //quad
+    0,1,
+    -1,1,
+    -1,0,
+    0,0,
+    0,1,
 ]);
 
 var gl: WebGL2RenderingContext | null;
@@ -101,7 +113,7 @@ function bindShaders(vSrc: string, fSrc: string) {
   }
   gl.useProgram(prog);
 }
-function bindBuffers() {
+function bindBuffers(img:ImageData) {
   if (!gl || !cns || !prog) {
     throw Error("no webgl");
   }
@@ -111,19 +123,28 @@ function bindBuffers() {
   const buff = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buff);
   gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
-
   //stride means the postion for the next vertex so here we have 4 data points each 4 bytes
   //we read from offset 0 here 2 elements
-  gl.vertexAttribPointer( 0, 2, gl.FLOAT, false, 4 * Float32Array.BYTES_PER_ELEMENT, 0);
+  gl.vertexAttribPointer( 0, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(0);
-  gl.vertexAttribPointer( 1, 2, gl.FLOAT, false, 4 * Float32Array.BYTES_PER_ELEMENT, 8);
+
+  const texCordData = new Float32Array(2*4*6)
+  const texCordBuf = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, texCordBuf)
+  gl.bufferData(gl.ARRAY_BUFFER,texCordData.byteLength, gl.DYNAMIC_DRAW)
+  gl.vertexAttribPointer( 1, 2, gl.FLOAT, false, 0,0);
   gl.enableVertexAttribArray(1);
   gl.bindVertexArray(null);
+  //@ts-ignore
+  texCordData.set(atlas['Z'],0);
+  //@ts-ignore
+  gl.bufferSubData(gl.ARRAY_BUFFER,0,texCordData)
 
   const texture = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, texture);
-  gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGB, 4, 4, 0, gl.RGB, gl.UNSIGNED_BYTE, pixels);
+  gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, atlas_width, atlas_height, 0, gl.RGBA, gl.UNSIGNED_BYTE, img);
   gl.generateMipmap(gl.TEXTURE_2D);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
 }
 
 var deg = 0.1;
@@ -147,23 +168,9 @@ function render() {
 
   //----- Bind shaders and buffers
 
-  //so unfirom should only be updated once in a render loop
-  let rad = (deg * Math.PI) / 180;
-  const rotLocal = gl.getUniformLocation(prog, "uRot");
-  gl.uniformMatrix2fv(
-    rotLocal,
-    false,
-    new Float32Array([
-      Math.cos(rad),
-      Math.sin(rad),
-      -1 * Math.sin(rad),
-      Math.cos(rad),
-    ])
-  );
-
   // Issue draw calls to render objects
   gl.bindVertexArray(vao1); //setting up vao for objects
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
   gl.bindVertexArray(null);
   //for other do another round
 
@@ -181,8 +188,7 @@ const loadAtlas = () => new Promise(resolve => {
     const img = await loadAtlas();
     init();
     bindShaders(vertShaderSrc, fragShaderSrc);
-    bindBuffers()
-    bindTex(img as ImageData)
+    bindBuffers(img as ImageData)
     requestAnimationFrame(render);
 })()
 
