af4bbea2ebc9ed9e520954c478ba5b5c62546678
Ezekiel Enns <Ezekiel.Enns@protonmail.com>
2023-11-01 11:21:00 -0300

    got a decient alg for atlasting

--- src/texture.ts
+++ src/texture.ts
@@ -4,6 +4,7 @@ two things this part can do
 1) generate a bitmap image
 2) generate a map of the image
 
+156 x 216
 Texture coord system
 0,1         1,1
 
@@ -14,10 +15,30 @@ interface AtlasMap {
     [glpyh:string]: Array<number>
 }
 
-function genMap(iW:number,iH:number,gW:number,gH:number):AtlasMap{
+interface Dim{
+    w:number,
+    h:number
+}
+
+//TODO follow a array/itter passeed to it
+function genMap({w:iW,h:iH}:Dim,{w:gW,h:gH}:Dim,iter:Array<string>):AtlasMap{
     let aMap:AtlasMap = {}
-    for (let i=0; i<96; i++){
+    let col = iW/gW
+    let row = iH/gH
+    for (let i=0; i<iter.length; i++){
+        let c = (i%col)
+        let r = Math.floor(i/col % row)
+        let left = c*gW/iW
+        let top = 1-(r*gH)/iH
+        let right = (c+1)*gW/iW
+        let bottom = 1-((r+1)*gH)-iH
+        aMap[iter[i]] = [
+            left,top,
+            right,top,
+            left,bottom,
+            right,bottom
+        ]
         
     }
-    return {}
+    return aMap
 }
