3aca835e57816281cb39e919612bb370dda7f5c4
Ezekiel Enns <Ezekiel.Enns@protonmail.com>
2023-11-07 13:37:45 -0400

    cleaning up dead code

--- src/texture.ts
+++ src/texture.ts
@@ -9,34 +9,6 @@ interface AtlasMap {
     [glpyh:string]: Array<number>
 }
 
-interface Dim{
-    w:number,
-    h:number
-}
-
-//TODO follow a array/itter passeed to it
-function genMap({w:iW,h:iH}:Dim,{w:gW,h:gH}:Dim,iter:Array<string>):AtlasMap{
-    let aMap:AtlasMap = {}
-    let col = iW/gW
-    let row = iH/gH
-    for (let i=0; i<iter.length; i++){
-        let c = (i%col)
-        let r = Math.floor(i/col % row)
-        let left = c*gW/iW
-        let top = 1-(r*gH)/iH
-        let right = (c+1)*gW/iW
-        let bottom = 1-((r+1)*gH)-iH
-        aMap[iter[i]] = [
-            left,top,
-            right,top,
-            left,bottom,
-            right,bottom
-        ]
-        
-    }
-    return aMap
-}
-
 export async function genAtlas(f:string,size=72):Promise<{img:ImageData, atlas:AtlasMap}> {
     const columns = 26; //26 cuase i felt like it (can be any value)
     let aMap:AtlasMap = {}
